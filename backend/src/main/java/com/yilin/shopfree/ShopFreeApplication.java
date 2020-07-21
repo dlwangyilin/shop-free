d43d019fb6e96ef7c017f7fcf1068bd8badab100
dlwangyilin <dlwangyilin@gmail.com>
2020-07-21 12:13:02 -0700

    Basic spring boot and jpa setup

---
+++ backend/src/main/java/com/yilin/shopfree/ShopFreeApplication.java
@@ -0,0 +1,13 @@
+package com.yilin.shopfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopFreeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yilin/shopfree/ShopfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yilin.shopfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
