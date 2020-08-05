c0c5e381a48bffa580f88652e57459dadaa747bf
dlwangyilin <Linyiwang_3218>
2020-08-05 16:50:49 -0700

    Set cors on backend and set service in frontend

--- backend/src/main/java/com/yilin/shopfree/dao/ProductRepository.java
+++ backend/src/main/java/com/yilin/shopfree/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.yilin.shopfree.dao;
 
 import com.yilin.shopfree.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
