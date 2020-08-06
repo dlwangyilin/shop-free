3edf02550b9aa1677c50b69b050c8b45ab894cca
dlwangyilin <Linyiwang_3218>
2020-08-06 12:47:03 -0700

    Expose category id in spring boot backend

--- backend/src/main/java/com/yilin/shopfree/config/MyDataRestConfig.java
+++ backend/src/main/java/com/yilin/shopfree/config/MyDataRestConfig.java
@@ -2,25 +2,54 @@ package com.yilin.shopfree.config;
 
 import com.yilin.shopfree.entity.Product;
 import com.yilin.shopfree.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         HttpMethod[] unsupportedMethods = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
 
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
-                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
-                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
+                .withItemExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods)))
+                .withCollectionExposure(((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods)));
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        List<Class<?>> entityClasses = new ArrayList<>();
+        // get the entity types for the entities
+        for (EntityType<?> entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+        // expose the entity ids for array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
